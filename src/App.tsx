bb6815a3ead01af76669d34b4b328cf44c89e9a9
Matthew Farquharson <matthewfarquharson@gmail.com>
2025-01-07 18:25:37 -0600

    Update text styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import Button from "./Button";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 min-h-screen bg-gray-100">
-      <h1 className="text-5xl font-bold mb-6">React + Tailwind Button</h1>
+    <div className="flex flex-col items-center justify-center  p-2 gap-4 min-h-screen bg-gray-100">
+      <h1 className="text-3xl text-center font-bold mb-6">
+        React + Tailwind Button
+      </h1>
       <Button text="Click Me" color="bg-blue-500 hover:bg-blue-700" />
       <Button text="Delete" color="bg-red-500 hover:bg-red-700" />
     </div>
